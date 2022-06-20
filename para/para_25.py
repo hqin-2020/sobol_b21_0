08c8f029ba05669da3bed27369b49d6d960ddae5
hqin-2020 <uctphqi@ucl.ac.uk>
2022-06-19 22:17:10 -0500

    1

--- para/para_25.py
+++ para/para_25.py
@@ -7,21 +7,32 @@ import pickle
 import os
 import time
 import sobol
-np.set_printoptions(suppress = True)
-from Minimization import minimization
 from concurrent.futures import ProcessPoolExecutor
+from Minimization import minimization
+np.set_printoptions(suppress = True)
+
 obs_series = pd.read_csv('data.csv', delimiter=',')
 obs_series = np.array(obs_series.iloc[:,1:]).T
 
-n_points = 80000
-sobol_sequence = sobol.sample(dimension=22, n_points=n_points)
-sobol_sequence[:,1:] = sobol_sequence[:,1:]*2 -1
+datadir = os.path.dirname(os.getcwd()) + '/sobol_sequence/'
+outputdir = os.path.dirname(os.getcwd()) + '/opt/'
+
+block = 25
+
+with open(datadir + 'sobol_'+str(block)+'.pkl','rb') as f:
+    sobol_sequence = pickle.load(f)
+
+n_points = sobol_sequence.shape[0]
 start = np.array_split(sobol_sequence, n_points)
 
+start_time = time.time()
 if __name__ == '__main__':
     with ProcessPoolExecutor() as pool:
         results = pool.map(minimization, start)
     results = [r for r in results]
 
-with open('data.pkl', 'wb') as f:
+run_time = time.time() - start_time
+print(block)
+print(run_time)
+with open(outputdir + 'output_'+str(block)+'.pkl', 'wb') as f:
        pickle.dump(results, f)
